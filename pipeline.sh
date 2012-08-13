9502bc76d1c231ece665b131ee2c9695b0cec7b4
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2012-08-13 14:05:59 +0200

    Added separate tmp for each run of the pipeline...
    Since any folder can only hold 100 000 files, this is necessary on UPPMAX. At leaste if more than one instance of the pipeline is to run at any on time. Also added functionality which ensures that this tmp directory is removed as long as the script dies gracefully. Scripts being kill with a -9 will still require a manual clean up however. But this should be rare as scancel on uppmax does not send this signal per default.

--- pipeline.sh
+++ pipeline.sh
@@ -24,6 +24,9 @@ PROJECT_NAME="test_pipeline"
 PATH_TO_FASTQ="/bubo/home/h10/joda8933/glob/private/pipelineTestFolder"
 GENOME_REFERENCE="/bubo/home/h10/joda8933/glob/Data/concat.fasta"
 DB_SNP="/bubo/home/h10/joda8933/glob/Data/dbSNP_all.vcf"
+# Note that the tmp folder needs to be placed in a location that can be reached from all nodes, e.g. glob
+# Note that $SNIC_TMP cannot be used since that will lose necessary data as the nodes/core switch.
+TMP=/bubo/home/h10/joda8933/glob/tmp/$SLURM_JOB_ID/
 
 #---------------------------------------------
 # Global variables
@@ -32,19 +35,33 @@ DB_SNP="/bubo/home/h10/joda8933/glob/Data/dbSNP_all.vcf"
 # Comment and uncomment DEBUG to enable/disable the debugging mode of the pipeline.
 DEBUG="-l DEBUG -startFromScratch"
 
-#Note that $SNIC_TMP cannot be used since that will lose necessary data as the nodes/core switch."
-TMP="-Djava.io.tmpdir=/bubo/home/h10/joda8933/glob/tmp"
+# Setup temporary directory for the the Qscript tmp files.
+# This will be removed as long as the script dies gracefully 
+# (if it is killed with a kill -9, manual clean up will have to be run...)
+function clean_up {
+	# Perform program exit housekeeping
+	rm -r ${TMP}
+	exit
+}
+
+if [ ! -d "${TMP}" ]; then
+   mkdir ${TMP}
+fi
+JAVA_TMP="-Djava.io.tmpdir="${TMP}
+
+#This will execute the removal of the tmp directory
+trap clean_up SIGHUP SIGINT SIGTERM
+
 QUEUE="${PWD}/gatk/dist/Queue.jar"
 SCRIPTS_DIR="${PWD}/gatk/public/scala/qscript/org/broadinstitute/sting/queue/qscripts"
-
 PATH_TO_BWA="/bubo/sw/apps/bioinfo/bwa/0.6.2/kalkyl/bwa"
 NBR_OF_BRA_THREADS=8
 
+# Setup directory structure
 RAW_BAM_OUTPUT="bam_files_raw"
 PROCESSED_BAM_OUTPUT="bam_files_processed"
 VCF_OUTPUT="vcf_files"
 
-# Setup directory structure
 if [ ! -d "${RAW_BAM_OUTPUT}" ]; then
    mkdir ${RAW_BAM_OUTPUT}
 fi
@@ -60,12 +77,12 @@ fi
 #------------------------------------------------------------------------------------------
 # fastq2bam - converts fastq files in input directory to unaligned bam files
 #------------------------------------------------------------------------------------------
-java ${TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/Fastq2Bam.scala --project ${PROJECT_NAME} -f ${PATH_TO_FASTQ} -outputDir ${RAW_BAM_OUTPUT}/ ${DEBUG} -jobRunner Drmaa -pid b2010028 --job_walltime 86400 -run
+java ${JAVA_TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/Fastq2Bam.scala --project ${PROJECT_NAME} -f ${PATH_TO_FASTQ} -outputDir ${RAW_BAM_OUTPUT}/ ${DEBUG} -jobRunner Drmaa -pid b2010028 --job_walltime 86400 -run
 
 #------------------------------------------------------------------------------------------
 # Data preprocessing
 #------------------------------------------------------------------------------------------
-java ${TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/DataProcessingPipeline.scala \
+java ${JAVA_TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/DataProcessingPipeline.scala \
 			  -R ${GENOME_REFERENCE} \
 			  --project ${PROJECT_NAME} \
 			  -i ${RAW_BAM_OUTPUT}/${PROJECT_NAME}.cohort.list \
@@ -83,7 +100,7 @@ java ${TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/DataProcessingPipeline.scala \
 #------------------------------------------------------------------------------------------
 # Run variant calling
 #------------------------------------------------------------------------------------------
-java ${TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/VariantCalling.scala \
+java ${JAVA_TMP} -jar ${QUEUE} -S ${SCRIPTS_DIR}/VariantCalling.scala \
 			  -R ${GENOME_REFERENCE} \
 			  --project ${PROJECT_NAME} \
 			  -i ${PROCESSED_BAM_OUTPUT}/${PROJECT_NAME}.cohort.list \
@@ -102,3 +119,6 @@ fi
 
 mv *.jobreport.* reports/
 
+# Remove the file temporary directory - otherwise it will fill up glob. And all the files which are required for
+# the pipeline to run are written to the pipeline directory.
+clean_up
